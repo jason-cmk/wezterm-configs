b3cc03d0714c9afdddc0cd3117239fba07f19e0c
Jason Kang <jason.kang@xero.com>
2024-07-28 01:56:39 +1200

    set ctrl-v to paste from OS

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,4 @@
 local wezterm = require('wezterm')
-
 local config = wezterm.config_builder()
 
 config.default_domain = 'WSL:Arch'
@@ -11,5 +10,14 @@ config.enable_tab_bar = false
 config.window_decorations = 'RESIZE'
 config.color_scheme = 'Catppuccin Mocha'
 config.window_background_opacity = 0.95
+
+local act = wezterm.action
+config.keys = {
+    -- paste from the clipboard
+    { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+
+    -- paste from the primary selection
+    { key = 'V', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
+}
 
 return config
