075d2e9e1b6f057c8687e0e5349f7490f7edabec
Jason Kang <jas.kang@gmail.com>
2026-05-19 10:05:56 +1200

    increase font size

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
 config.font = wezterm.font('CaskaydiaCove Nerd Font')
-config.font_size = 13
+config.font_size = 15
 
 config.enable_tab_bar = false
 config.window_decorations = 'RESIZE'
